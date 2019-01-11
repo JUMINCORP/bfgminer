6f1b02493b74d86dbfecd3ecd34e4d2e03ff0133
Steve Darveau <steve@darvs.net>
2019-01-11 11:49:56 -0500

    - Working solution, without AMD-APP-SDK, but with some COPYing from amdgpu-pro

--- docker/ubuntu/build.sh
+++ docker/ubuntu/build.sh
@@ -3,12 +3,7 @@ NAME=build-bfgminer
 USER=$(id -u)
 GROUP=$(id -g)
 
-PWD=$(cd $(dirname "$0") && pwd)
+cd $(dirname "$0")
 
 echo Building docker image...
-docker build -t bfg-builder . || exit 1
-
-#CONTAINERS=$(docker ps -a -f name=${NAME} -q)
-#[ -z ${CONTAINERS} ] || docker rm ${CONTAINERS}
-
-#docker run --name ${NAME} -a stdin -a stdout -a stderr -u ${USER}:${GROUP} -v ${PWD}/../../:/bfg: -w /bfg/ bfg-builder:latest /bin/bash -c "find . && echo building && ./autogen.sh && sh reconfigure-combo.sh && make V=0"
+docker build -t eqb-bfg-amd . || exit 1
--- docker/ubuntu/run.sh
+++ docker/ubuntu/run.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-docker run -t -a stdout --network host --device=/dev/kfd --device=/dev/dri -w /bfg/ bfg-builder:latest sh -c "/bfg/bfgminer -u equibit -p equibit --generate-to tq1qhc6eardg9ae8q8k86988hg9vl9c6e3lukwm7qm --coinbase-sig \"This is a testnet Equibit block\"  -D -o http://localhost:18331 --no-default-config -S auto"
+docker run -t -a stdout --network host --device=/dev/kfd --device=/dev/dri -w /bfg/ eqb-bfg-amd:latest sh -c "/bfg/bfgminer -u equibit -p equibit --generate-to tq1qhc6eardg9ae8q8k86988hg9vl9c6e3lukwm7qm --coinbase-sig \"This is a testnet Equibit block\" -D -o http://localhost:18331 --no-default-config -S auto"
+#docker run -t -a stdout --network host --security-opt seccomp:unconfined --user 1000:44 --device=/dev/kfd --device=/dev/dri -w /bfg/ eqb-bfg-amd:latest sh -c "clinfo"
